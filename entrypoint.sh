d8b2da08ae12da1226f04c2b4091893541e7b156
melanieziur <72086257+melanieziur@users.noreply.github.com>
2022-10-17 11:48:07 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
 
-case "${GITHUB_EVENT_NAME}" in
+case "${GITHUB_EVENT_NAME}" in 
+workflow_dispatch)
+        ;;
     push)
         git push --all target
         git push --tags target
