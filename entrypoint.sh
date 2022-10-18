da7333d1dc0d84e4c862568629f9e7a82cbed6e7
melanieziur <72086257+melanieziur@users.noreply.github.com>
2022-10-18 17:44:17 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,2 @@
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
-
-case "${GITHUB_EVENT_NAME}" in 
-workflow_dispatch)
-        echo workflow dispatch recognized
-        git push --all target
-        git push --tags target
-        ;;
-    *)
-        echo did not work
-        break
-        ;;
-esac
+git push --mirror target
